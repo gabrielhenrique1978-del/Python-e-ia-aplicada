3d835d82f41b49647522bf1786c9b37f32eec55a
Gabriel Henrique <gabriel.henrique1978@gmail.com>
2026-07-22 18:36:29 -0300

    meu primeiro projeto python

---
+++ teste.py
@@ -0,0 +1,13 @@
+nome=input ('qual seu nome?')
+print(nome,'prazer em te conhecer!')
+Marca=input('qual nome da sua marca?')
+print(Marca,'é uma marca legal!')
+atuacao=input('qual área de atuação da marca?')
+print(atuacao,'é uma área de atuação interessante!')
+logo=input('sua marca tem logo?')
+if logo=='sim':
+    print(logo,',que bom!')
+else:
+    print(logo,'sem problemas, podemos criar um logo para sua marca!')
+viabilidade=input('aqui temos busca de viabilidade gratis,posso realizar?')
+print(viabilidade,'ok, vamos lá!')
